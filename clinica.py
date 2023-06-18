f10e6eeb08e8a850307f6d617243c76959a6cbfb
Luis Neto <santosneto.luis@gmail.com>
2023-06-18 14:20:40 -0300

    Finished Project

--- clinica.py
+++ clinica.py
@@ -1,11 +1,9 @@
-patients = [
-    {"nome": "Luis", "telefone": "98991842214"},
-    {"nome": "Marcos", "telefone": "98991843321"},
-    {"nome": "Danilo", "telefone": "98987842214"}
-    ]
-schedules = [
-    {"paciente": "Luis", "dia": "12/07", "hora": "14:50", "especialidade": "Cardiologista"}
-]
+import datetime
+import json
+import os
+
+patients = []
+schedules = []
 
 def menu():
     print("\n=========Clinica Ágil=========")
@@ -27,11 +25,11 @@ def registerPatient():
     for i in patients:
         if i.get("telefone") == patient["telefone"]:
             print("Paciente já cadastrado")
+            return
             break
-        else:
-            patients.append(patient)
-            print("Paciente cadastrado com sucesso!")
-            break
+
+    patients.append(patient)
+    print("Paciente cadastrado com sucesso!")
 
 def listArray(list, type):
     index = 0
@@ -47,25 +45,100 @@ def listArray(list, type):
           print(f"{key.title()}: {value}", end=" - ")
         print("\n")
 
+def validateDate(data):
+    currencyDate = datetime.datetime.now().date()
+
+    formatedDate = datetime.datetime.strptime(data, '%d/%m/%Y').date()
+    if formatedDate > currencyDate:
+        return False
+    else:
+        print("Escolha uma data posterior a atual!")
+        return True
+
+
 def consultationAppointments():
+    if len(patients) == 0:
+        print("Não há pacientes cadastrados!")
+        return
+
     listArray(patients, "pacientes")
 
     print("Selecione o paciente para agendar a consulta")
     option = int(input("Número do paciente: "))
 
-    consulta = {}
-    consulta['paciente'] = patients[option]["Nome"]
-    consulta['dia'] = input("Digite o dia da consulta (dd/mm): ")
-    consulta['hora'] = input("Digite a hora da consulta (00:00): ")
-    consulta['especialidade'] = input("Digite a especialidade: ")
+    if option < 0 or option >= len(patients):
+        print("Opção inválida!")
+    else: 
+        appointment = {}
+        appointment["paciente"] = patients[option]["nome"]
+        
+        run = True
 
-    schedules.append(consulta)
+        while run:
+            appointment["data"] = input("Digite o data da consulta (dd/mm/aaaa): ")
+            run = validateDate(appointment["data"])
+            
+        appointment["hora"] = input("Digite a hora da consulta (00:00): ")
+        appointment["especialidade"] = input("Digite a especialidade: ")
 
-    print(schedules)
+        for scheduling in schedules:
+            if scheduling["data"] == appointment["data"] and scheduling["hora"] == appointment["hora"]:
+                print("Já existe uma consulta agendada neste horário!")
+                break
+        else:
+            schedules.append(appointment)
+            print("Consulta agendada com sucesso!")
+
+        print(schedules)
 
 def cancellationAppointments():
     listArray(schedules, "agendamentos")
+
+    if len(schedules) == 0:
+        print("Não há nenhuma consulta agendada!")
+        return
+
+    option = int(input("Escolha um agendamento: "))
+
+    for key, value in schedules[option].items():
+        if key == "data" or key == "hora":
+            print(f"{key.title()}: {value.title()}")
+
+    choose = input("Você deseja cancelar a consulta (s/n): ").strip().lower()
+
+    if choose == "s" or choose == "sim":
+        schedules.pop(option)
+        print("Ok! Consulta cancelada!")
+    elif choose == "n" or choose == 'nao' or choose == "não":
+        print("Ok! Consulta continua agendada.")
+    else:
+        print("Opção inválida! Digite uma opção válida.")
+
+def saveData():
+    clinic = {
+        "patients": patients,
+        "schedules": schedules
+    }
+
+    with open('database.json', 'w') as archive:
+        json.dump(clinic, archive)
+
+def loadData():
+    if os.path.getsize('database.json') == 0:
+        patients = []
+        schedules = []
+        return patients, schedules
     
+    with open("database.json", "r") as archive:
+        data = json.load(archive)
+
+    patients = data["patients"]
+    schedules = data["schedules"]
+
+    return patients, schedules
+
+
+patients, schedules = loadData()
 
 while True:
     menu()
@@ -84,4 +157,5 @@ while True:
         cancellationAppointments()
     
     elif option == 0:
+        saveData()
         break
